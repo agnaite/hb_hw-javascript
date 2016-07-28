9bbe543388ea261f87a7bc9b1fd182e0ead576d4
Agnė Klimaitė <agnaite@me.com>
2016-07-27 22:01:03 -0700

    converted python function to js

---
+++ melon_counting.js
@@ -0,0 +1,24 @@
+var melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+
+function count_melons(melonList) {
+  var melonCounts = {};
+
+  for (var i = 0; i < melonList.length; i++) {
+    if (melonList[i] in melonCounts) {
+      melonCounts[melonList[i]] += 1;
+    } else {
+      melonCounts[melonList[i]] = 1;
+    }
+  }
+  return melonCounts;
+}
